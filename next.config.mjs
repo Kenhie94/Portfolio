c3cb2c3b9b251027c2e4a6871c7610b22163c336
Kenjy Jap <kenhiebusiness@gmail.com>
2025-02-21 15:17:22 -0800

    added env to config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_GITHUB_USERNAME: process.env.NEXT_PUBLIC_GITHUB_USERNAME,
+    GITHUB_TOKEN: process.env.GITHUB_TOKEN, // Secure server-side variable
+  },
+};
 
 export default nextConfig;
