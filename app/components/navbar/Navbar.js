2c58576fe141c443cf2c2f552b8e208031bf638b
Kenjy Jap <kenhiebusiness@gmail.com>
2025-03-22 11:23:41 -0700

    centered navbar

--- app/components/navbar/Navbar.js
+++ app/components/navbar/Navbar.js
@@ -2,30 +2,31 @@
 
 import React from "react";
 import Link from "next/link";
-import { FaHome, FaUser, FaCode, FaFileAlt, FaStar, FaCodeBranch } from "react-icons/fa";
+import { FaHome, FaUser, FaCode, FaFileAlt, FaStar, FaCodeBranch, FaTimes, FaBars } from "react-icons/fa";
 import { GrLanguage } from "react-icons/gr";
 import "./navbarstyle.css";
 
 export default function Navbar() {
+
   const scrollToSection = (id) => {
     const section = document.getElementById(id);
     if (section) {
       section.scrollIntoView({ behavior: "smooth" });
     }
+    setMenuOpen(false);
   };
 
   return (
     <header className="navbarContainer">
       <nav className="navbar navbar-expand-lg px-4">
-        <div className="container-fluid d-flex justify-content-between align-items-center">
+        <div className="container-fluid d-flex align-items-center justify-content-between">
           {/* Left Side - Brand Name */}
-          <Link className="navLink fs-1 fw-bold" href="/" onClick={() => scrollToSection("about")} style={{ cursor: "pointer" }}>
+          <Link className="navbar-brand navLink fs-1 fw-bold" href="/" onClick={() => scrollToSection("about")} style={{ cursor: "pointer" }}>
             Kenjy Jap
           </Link>
-
           {/* Right Side - Navigation Links */}
-          <div className="d-flex">
-            <ul className="navbarNav d-flex flex-row gap-4 fs-4 fw-bold">
+          <div className="d-flex align-items-center">
+            <ul className="navbar-nav navbarNav d-flex flex-row gap-4 fs-4 fw-bold align-items-center">
               <li className="nav item">
                 <Link className="navLink" href="#hero">
                   <FaHome className="pb-1" onClick={() => scrollToSection("hero")} style={{ cursor: "pointer" }} /> Home
